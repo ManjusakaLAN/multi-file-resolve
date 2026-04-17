b10482d2e411acb90c53ff41f0345390562fd469
zhushijie <654414824@qq.com>
2026-04-17 18:03:01 +0800

    完成以下功能开发： 1. 刻写版本 26 4 17

--- services/auth/login_service.py
+++ services/auth/login_service.py
@@ -40,7 +40,7 @@ def _generate_tokens(user: User) -> Tuple[str, str]:
 
 
 class LoginService:
-    def __init__(self, db: AsyncSession, redis: Redis |  None):
+    def __init__(self, db: AsyncSession, redis: Redis):
         self.db = db
         self.redis = redis
 
